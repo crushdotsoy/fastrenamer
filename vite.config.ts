c5ded6e23666a7e8a3f9a4f27ab462a26f3ddbba
Mário Gomes <mario@gomes.lol>
2026-04-10 10:14:38 +0100

    fix: stabilize release builds in CI

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,6 @@ export default defineConfig(({ mode }) => ({
                 preload: 'electron/preload.ts',
               },
             },
-            renderer: {},
           }),
         ]),
   ],
